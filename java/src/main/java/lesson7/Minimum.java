55bf87569afd3bb607e8de9188507d8b17366dc4
SineokyGK <sineoky@gmail.com>
2021-02-25 09:32:09 +0300

    homework7

---
+++ java/src/main/java/lesson7/Minimum.java
@@ -0,0 +1,15 @@
+package lesson7;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.io.Serializable;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Minimum implements Serializable {
+    @JsonProperty(value = "Value")
+    public Double value;
+    @JsonProperty(value = "Unit")
+    public String unit;
+    @Override
+    public String toString() {return "Минимальная температура: "+value+" " +unit;}
+}
---
+++ java/src/main/java/lesson7/Periods.java
@@ -0,0 +1,6 @@
+package lesson7;
+
+public enum Periods {
+    NOW,
+    IN5DAYS
+}
